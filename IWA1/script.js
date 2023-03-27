5155067a5f5e2b0dfc603983b5e4e229b756b44c
Akido123 <117109568+Akido123@users.noreply.github.com>
2023-03-27 17:47:16 +0200

    Index - has a header with a title, main content containing the input value and buttons and a footer with a link to Tally Count Style - controls the general color, fonts and transitions Script - adds functions to buttons to add and subtract from Input

--- IWA1/script.js
+++ IWA1/script.js
@@ -1,6 +1,6 @@
 const MAX_NUMBER = 15
 const MIN_NUMBER = -5
-const STEP_AMOUNT = 5
+const COUNT_AMOUNT = 5
 
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
@@ -9,7 +9,7 @@ const add = document.querySelector('[data-key="add"]')
 console.log(number, subtract, add)
 
 const subtractHandler = () => {
-  const newValue = parseInt(number.value) - STEP_AMOUNT
+  const newValue = parseInt(number.value) - COUNT_AMOUNT
   number.value = newValue
 
   if(add.disabled === true) {
@@ -22,7 +22,7 @@ const subtractHandler = () => {
 }
 
 const addHandler = () => {
-  const newValue = parseInt(number.value) + STEP_AMOUNT
+  const newValue = parseInt(number.value) + COUNT_AMOUNT
   number.value = newValue
 
   if(subtract.disabled === true){
